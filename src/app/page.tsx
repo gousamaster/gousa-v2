e7d7925092115eed47c644f805e5215f973666b2
Adhemar Claure <adhemarangelclaureflores@gmail.com>
2026-02-03 10:50:25 -0400

    feat: update Next.js configuration and refactor Home component layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { AuthLayout } from "@/components/auth/shared/auth-layout";
 import { SignInForm } from "@/components/auth/sign-in-form";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div>
+      <AuthLayout>
         <SignInForm />
-      </main>
+      </AuthLayout>
     </div>
   );
 }
